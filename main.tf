7d03bb5f4e619f1e8341dc9f20522b40c0da9e5b
sravan224 <125477355+sravan224@users.noreply.github.com>
2023-08-23 21:00:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-resource "local_file" "myself" {
-  filename = "usage.txt"
-  content  = "new content"
+variable "filename" {
+default =""root/pets.txt"
+}
+variable "content" {
+default ="we love pets!"
+}
+variable "prefix" {
+default ="MR"
+}
+variable "separator" {
+default ="."
+}
+variable "length" {
+default ="1"
 }
